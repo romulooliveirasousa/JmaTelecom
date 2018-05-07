928f79875e78809f73c50079437a172d29e6cf33
romulo <romulo.rmos@hotmail.com>
2018-05-07 12:26:57 -0300

    Initial commit

---
+++ planos.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" class="wf-fontawesome-n4-active wf-active" lang="pt-br">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+    <title>Sulnet </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=no">
+    <link href="css/css.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+    <script src="js/webfontloader.js" style=""></script><script src="js/7f02b4d775.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel='icon' href='midia/ico.png'/>
+
+
+    <!-- Page MENU TOPO -->
+    <link rel="stylesheet" href="css/7f02b4d775.css" media="all">
+    <script type="text/javascript" charset="UTF-8" src="css/common.js"></script>
+    <script type="text/javascript" charset="UTF-8" src="js/util.js"></script>
+    <script type="text/javascript" charset="UTF-8" src="js/stats.js"></script>
+</head>
+<body>
+<div class="topo bg-azul">
+    <div class="col-md-6 col-sm-12">
+        <a href="#" class="amarelo plano-topo">
+            Para Você Urbano
+        </a> <span> | </span>
+        <a href="#" class="verde plano-topo">
+            Para Você Rural
+        </a> <span> | </span>
+        <a href="#" class="vermelho plano-topo">
+            Para Sua Empresa
+        </a>
+    </div>
+    <div class="col-md-6 col-sm-12">
+        <a class="youtube externo pull-right" href="#">
+            <i class="fa fa-youtube" aria-hidden="true"></i>
+        </a>
+        <a class="facebook externo pull-right" href="#">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+        </a>
+        <a class="envelope pull-right" href="#" style="padding: 0 15px">
+            <i class="fa fa-envelope-o" aria-hidden="true"></i>
+        </a>
+        <div class="clearfix"></div>
+    </div>
+</div>
+
+
+<!-- Navigation -->
+<nav class="navbar menuNavBar" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header col-lg-2 col-md-4">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menutopo">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php" title="Sulnet">
+            <img src="imagens/logo-tema-padrao.png" alt="Sulnet" class="img-responsive">
+        </a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="col-lg-10 col-md-8">
+
+        <div class="collapse navbar-collapse" id="menutopo">
+            <ul class="nav navbar-nav col-xs-12">
+                <li>
+                    <a href="quem_somos.php">Nossa Empresa</a>
+                </li>
+                <li>
+                    <a href="#">Serviços</a>
+                </li>
+                <li>
+                    <a href="planos.php">Planos</a>
+                </li>
+                <li>
+                    <a href="atuacao.php">Área de Cobertura</a>
+                </li>
+
+                <li>
+                    <a href="contato.php">Contato</a>
+                </li>
+
+
+                <li class="pull-right">
+                    <a href="http://www.sntprovedores.com.br/central.php" class="externo">
+                        <i class="icone webmail"></i><strong>Área do Cliente</strong></a>
+                </li>
+
+
+
+            </ul>
+        </div>
+    </div>
+    <!-- /.navbar-collapse -->
+
+</nav>
+<div class="breadcrumb">
+    <nav class="container">
+        <a href="index.php" title="Home">Home</a>
+        <i class="fa fa-angle-right" aria-hidden="true"></i>
+        Nossos Planos
+    </nav>
+</div>
+<div class="box-titulo fill" style="background-image:url('midia/titulo-hospedagem.jpg');">
+    <div class="container text-center">
+        <div class="meio">
+            <h1>NOSSOS PLANOS</h1>
+        </div>
+    </div>
+</div>
+<!--  -->
+<div class="divisor">
+    <div class="col-md-4 bg-verde"></div>
+    <div class="col-md-4 bg-vermelho"></div>
+    <div class="col-md-4 bg-amarelo"></div>
+</div>
+<div class="container servicos" style="margin-bottom: 0">
+
+    <div class="row">
+
+        <div class="servicosHome">
+
+            <div class="container"><br><br>
+
+                <div class="col-md-4 text-center" >
+                    <div class='package'>
+                        <div class='name'>Avançado</div>
+                        <div class='price'>R$ 85,00</div>
+                        <div class='trial'>2 MB</div>
+                        <hr>
+                        <ul>
+                            <li><span class="fa fa-desktop"></span> Navegação: 2 MEGA </li>
+                            <li><span class="fa fa-download"></span> Download: 512kbps </li>
+                            <li><span class="fa fa-upload"></span> Upload: 128kbps</li>
+                            <li><span class="fa fa-exchange"></span> Tx Transf: até 50 KB/s</li>
+                            <li><span class="fa fa-globe"></span> Suporte Ilimitado</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-4 text-center" >
+                    <div class='package'>
+                        <div class='name'>Profissional</div>
+                        <div class='price'>R$ 99,00</div>
+                        <div class='trial'>3 MB</div>
+                        <hr>
+                        <ul>
+                            <li><span class="fa fa-desktop"></span> Navegação: 3 MEGA </li>
+                            <li><span class="fa fa-download"></span> Download: 512kbps </li>
+                            <li><span class="fa fa-upload"></span> Upload: 128kbps</li>
+                            <li><span class="fa fa-exchange"></span> Tx Transf: até 50 KB/s</li>
+                            <li><span class="fa fa-globe"></span> Suporte Ilimitado</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4 text-center" >
+                    <div class='package'>
+                        <div class='name'>Exclusivo</div>
+                        <div class='price'>R$ 160,00</div>
+                        <div class='trial'>5 MB</div>
+                        <hr>
+                        <ul>
+                            <li><span class="fa fa-desktop"></span> Navegação: 5 MEGA </li>
+                            <li><span class="fa fa-download"></span> Download: 512kbps </li>
+                            <li><span class="fa fa-upload"></span> Upload: 128kbps</li>
+                            <li><span class="fa fa-exchange"></span> Tx Transf: até 50 KB/s</li>
+                            <li><span class="fa fa-globe"></span> Suporte Ilimitado</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <br/><br/><br/>
+        </div>
+        <div class="clearfix"></div>
+
+    </div>
+
+</div>
+
+
+
+<div class="divisor">
+    <div class="col-md-4 bg-verde"></div>
+    <div class="col-md-4 bg-vermelho"></div>
+    <div class="col-md-4 bg-amarelo"></div>
+
+</div>
+
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <img src="imagens/logo-tema-padrao.png" class="brand"/>
+                Desde 2003, aproximamos pessoas por meio de serviços de alta qualidade, oferecendo soluções inteligentes para as mais variadas necessidades.
+
+                <br /><br />
+                <a href="#" class="externo bg-amarelo social facebook">
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                </a>
+                <a href="#" class="externo bg-amarelo social youtube">
+                    <i class="fa fa-youtube" aria-hidden="true"></i>
+                </a>
+            </div>
+            <div class="col-md-3">
+                <h2 class="verde">Institucional           </h2>
+                <a href="#"><i class="icone arrow-right"></i>Quem Somos</a><br/>
+                <a href="atuacao.htm"><i class="icone arrow-right"></i>Área de Atuação</a><br/>
+
+            </div>
+            <div class="col-md-3">
+                <h2 class="verde">Serviços           </h2>
+                <a href="#"><i class="icone arrow-right"></i>Para Você Urbano</a><br/>
+                <a href="#"><i class="icone arrow-right"></i>Para Você Rural</a><br/>
+                <a href="#"><i class="icone arrow-right"></i>Para Sua Empresa</a><br/>
+
+
+            </div>
+            <div class="col-md-3">
+                <h2 class="vermelho">Entre em Contato           </h2>
+
+                <div class="row">
+
+                    <div class="col-md-9">
+
+                        <strong>(63) 3602 - 1385</strong><br
+                    </div>
+
+                    Rua Barão do Rio Branco Nº 554
+                    Paraíso do Tocantins - TO
+                    <div class="clear-fix"></div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<div class="copyright">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                2017 Todos os Direitos Reservados - Sulnet </div>
+        </div>
+    </div>
+</div>
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="http://maps.google.com/maps/api/js?sensor=true" type="text/javascript"></script>
+<script src="js/uteis.js"></script>
+
+
+
+</body>
+
+</html>
